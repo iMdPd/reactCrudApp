902adcf6ca2c67d0e114921538762f6a7aec9a76
Mariusz Politowicz <politowiczmar@gmail.com>
2023-03-15 22:48:17 +0100

    add provider component in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { App } from "./App";
+import { store } from "./redux/store";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
