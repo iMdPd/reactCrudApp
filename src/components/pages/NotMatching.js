c24299d18e6c74c8d85001758cb345387edf11c3
Mariusz Politowicz <politowiczmar@gmail.com>
2023-03-16 23:19:08 +0100

    Add Antd package

--- src/components/pages/NotMatching.js
+++ src/components/pages/NotMatching.js
@@ -1,7 +1,12 @@
+import { Button, Result } from "antd";
+
 export const NotMatching = () => {
   return (
-    <>
-      <h1>NotMatching</h1>
-    </>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={<Button type="primary">Back Home</Button>}
+    />
   );
 };
