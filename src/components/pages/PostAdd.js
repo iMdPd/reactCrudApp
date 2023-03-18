2e69c36c7f3ecb5910540a74db9e6d38b46eddd5
Mariusz Politowicz <politowiczmar@gmail.com>
2023-03-18 17:44:35 +0100

    Add form in PostAdd component

--- src/components/pages/PostAdd.js
+++ src/components/pages/PostAdd.js
@@ -1,7 +1,40 @@
+import { Col, Form, Button } from "react-bootstrap";
+
 export const PostAdd = () => {
   return (
-    <>
-      <h1>PostAdd</h1>
-    </>
+    <div className="d-flex justify-content-center">
+      <Col md={8}>
+        <h1>Add Post</h1>
+        <Form.Group className="mb-3" controlId="addPostForm">
+          <Col md={6}>
+            <Form.Label>Title :</Form.Label>
+            <Form.Control
+              className="mb-3"
+              type="text"
+              placeholder="Enter post Title"
+            />
+            <Form.Label>Author :</Form.Label>
+            <Form.Control
+              className="mb-3"
+              type="text"
+              placeholder="Enter post Author"
+            />
+            <Form.Label>Published :</Form.Label>
+            <Form.Control
+              className="mb-3"
+              type="text"
+              placeholder="Enter published date"
+            />
+          </Col>
+          <Form.Label>Short Description :</Form.Label>
+          <Form.Control className="mb-3" as="textarea" rows={2} />
+          <Form.Label>Main Content :</Form.Label>
+          <Form.Control as="textarea" rows={6} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Col>
+    </div>
   );
 };
