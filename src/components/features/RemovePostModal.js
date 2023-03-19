c98d20c1881e5db5437270f881e65b5505da1264
Mariusz Politowicz <politowiczmar@gmail.com>
2023-03-19 17:20:21 +0100

    Fix warning thru passing params to Modal

--- src/components/features/RemovePostModal.js
+++ src/components/features/RemovePostModal.js
@@ -1,33 +1,35 @@
 import { Modal, Button } from "react-bootstrap";
 
-export const RemovePostModal = (props) => {
+export const RemovePostModal = ({ onRemove, onCancel, ...props }) => {
   return (
-    <Modal
-      {...props}
-      size="md"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Are you sure?
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p className="my-0">
-          This decision will completly remove this post from app. <br />
-          After clicking Remove button, there is no way back! <br />
-          Are you really sure?
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={props.onCancel}>
-          Cancel
-        </Button>
-        <Button variant="danger" onClick={props.onRemove}>
-          Remove
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Are you sure?
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p className="my-0">
+            This decision will completly remove this post from app. <br />
+            After clicking Remove button, there is no way back! <br />
+            Are you really sure?
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={onCancel}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={onRemove}>
+            Remove
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
